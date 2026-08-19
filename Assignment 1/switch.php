208980d884e9484780404296420aa5fb2abae8cf
JykeDeala <dealajyke@gmail.com>
2026-08-19 17:34:30 +0800

    Add switch statement for hero classification

---
+++ Assignment 1/switch.php
@@ -0,0 +1,20 @@
+<?php
+$hero = "Layla";
+
+switch ($hero) {
+    case "Layla":
+        echo "Marksman Hero";
+        break;
+
+    case "Tigreal":
+        echo "Tank Hero";
+        break;
+
+    case "Eudora":
+        echo "Mage Hero";
+        break;
+
+    default:
+        echo "Unknown Hero";
+}
+?>
