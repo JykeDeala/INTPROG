1cff2f8c024a6509bc9c1c9b9c583460a7ab88c3
JykeDeala <dealajyke@gmail.com>
2026-09-01 18:34:52 +0800

    Add JSON decoding and output for username and password

--- Exercise 1/lab4_input.php
+++ Exercise 1/lab4_input.php
@@ -1,1 +1,11 @@
+<?php
 
+$json = '{"username":"JykePogi","password":"12345678"}';
+
+$data = json_decode($json, true);
+
+echo "Username: " . $data["username"];
+echo "<br>";
+echo "Password: " . $data["password"];
+
+?>
