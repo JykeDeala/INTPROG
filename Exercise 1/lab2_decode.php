2642f0181086d55cd2e4813b9881a8fd6d6d47d4
JykeDeala <dealajyke@gmail.com>
2026-09-01 18:34:15 +0800

    Decode JSON to object and array in lab2_decode.php

--- Exercise 1/lab2_decode.php
+++ Exercise 1/lab2_decode.php
@@ -1,1 +1,14 @@
+<?php
 
+$json = '{"name":"Jyke Rafael A. Deala","age":20,"email":"itsmejykey@gmail.com"}';
+
+$object = json_decode($json);
+
+$array = json_decode($json, true);
+
+echo "Object: " . $object->name;
+echo "<br>";
+
+echo "Array: " . $array["email"];
+
+?>
