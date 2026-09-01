f734c7884c85103a602c1a3b7b1c700487e6a21f
JykeDeala <dealajyke@gmail.com>
2026-09-01 18:34:31 +0800

    Update lab3_api.php

--- Exercise 1/lab3_api.php
+++ Exercise 1/lab3_api.php
@@ -1,1 +1,14 @@
+<?php
 
+header('Content-Type: application/json');
+
+$user = array(
+    "id" => 1,
+    "name" => "Jyke Rafael A. Deala",
+    "email" => "itsmejykey@gmail.com",
+    "status" => "active"
+);
+
+echo json_encode($user);
+
+?>
